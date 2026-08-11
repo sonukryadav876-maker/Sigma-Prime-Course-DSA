6bfb209bd90b5846effe19b0d53db6e83cb075c5
Shatrudhan Kumar <sonukryadav876@gmail.com>
2026-08-11 11:08:59 +0530

    Run

---
+++ Kallua.java
@@ -0,0 +1,10 @@
+public class Kallua {
+    public static void main(String[] args) {
+        
+        int a = 36;
+        int b = 90;
+
+        int Mul = a * b;
+        System.out.println(Mul);
+    }
+}
