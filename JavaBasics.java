2a5ae62aee51db82e2f34c769cf50a705c6ddec7
Shatrudhan Kumar <sonukryadav876@gmail.com>
2026-08-11 22:07:16 +0530

    A,S

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String none = "tony stark";
+        System.out.println(none);
+    }
+}
