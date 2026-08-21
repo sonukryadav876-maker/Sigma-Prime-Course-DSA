8d12f17159477088dd90b990c186b4e6b0c89b74
Sonu kumar yadav <sonukryadav876@gmail.com>
2026-08-21 10:45:50 +0530

    Aarti.java

---
+++ Aarti.java
@@ -0,0 +1,18 @@
+public class Aarti {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        int number = 25;
+        //long
+        //double
+        short n = 240;
+        System.out.println(number);
+        System.out.println(n);
+        
+
+    }
+    
+}
