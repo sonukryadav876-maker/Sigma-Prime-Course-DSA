c384ff8dd131ecb94f0adc69482352de3dd4b037
Sonu kumar yadav <sonukryadav876@gmail.com>
2026-08-21 11:20:21 +0530

    scanner.java

---
+++ scanner.java
@@ -0,0 +1,12 @@
+import java. util.*;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+        String name = sc. nextLine();
+        System.out.println(name);
+    }
+    
+}
